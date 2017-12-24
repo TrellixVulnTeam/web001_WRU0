1f9c195c0265901ba7fae5c282a55456f20d6632
alan077 <370220760@qq.com>
2017-12-24 18:40:10 +0800

    注册页面修改表单

--- polls/models.py
+++ polls/models.py
@@ -8,3 +8,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choic_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+class member(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    pwd = models.CharField(max_length=30)
+    phone = models.IntegerField()
