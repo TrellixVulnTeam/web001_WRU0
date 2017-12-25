5142247b03e435207ce732e5d41cf09174bed54c
alan077 <370220760@qq.com>
2017-12-25 22:26:28 +0800

    mode

--- home/CMDB/models.py
+++ home/CMDB/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class member(models.Model):
+class PrivilegesInfo(models.Model):
+    GroupID = models.CharField(max_length=20)
+    Group = models.CharField(max_length=20)
+class UserInfo(models.Model):
     UserID = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
-    pwd = models.CharField(max_length=30)
-    phone = models.IntegerField()
+    GroupID = models.ForeignKey('PrivilegesInfo')
+    LoginId = models.CharField(max_length=30)
+    Pwd = models.CharField(max_length=30)
+    Name = models.CharField(max_length=20)
+    Sex = models.BooleanField(default=True)
+    Phone = models.IntegerField()
+    Address =models.CharField(max_length=100)
+    CreateDate = models.DateTimeField(auto_now_add=True)
+    LastLogin = models.DateTimeField(auto_now=True)
+class LoginLog(models.Model):
+    Id = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey('UserInfo')
+    LoginIP = models.CharField(max_length=30)
+    LoginStatus = models.BooleanField()
+    LoginFail = models.IntegerField()
+    LoginTime = models.DateTimeField(auto_now_add=True)
